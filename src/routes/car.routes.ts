45aa5ee1573a5d1f68a8b9588c57f8712f8cde4a
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-05-17 02:44:58 +0700

    refactor: Create method GET Specific data

--- src/routes/car.routes.ts
+++ src/routes/car.routes.ts
@@ -14,3 +14,13 @@ router.get("/", (req: Request, res: Response) => {
 
   });
 });
+
+// GET specific car by id
+router.get("/:id", (req: Request, res: Response) => {
+  const getId: number = Number(req.params.id);
+  const carById = cars.find(({id}) => id === getId);
+  res.status(200).json({
+    message: "Get specific car by id",
+    data: carById,
+  });
+})
