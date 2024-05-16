df6584d499e5f7720da854ab128e5206db67ff3a
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-05-17 02:19:53 +0700

    refactor: add server port

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+
+const port = 3000;
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message : 'Hello World!'});
+});
+
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`API Ruuning on port ${port}`);
+});
