8524cf1898136a9ada95f14ea1cf44343d47d81b
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-06-01 00:53:17 +0700

    refactor: service repository pattern

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -1,11 +1,10 @@
-import { CarsModel } from "../models/CarsModel";
 import { Request, Response } from "express";
 import { mUpload } from "../middlewares/multer";
 import cloudinary from "../config/cloudinary";
+import { getAllCars, getCarId, postCar, putCar, removeCar } from "../services/carsSevice";
 
-
-export const getCars =  async (req: Request, res: Response) => {
-  const cars = await CarsModel.query();
+export const getCars =  async (_req: Request, res: Response) => {
+  const cars = await getAllCars();
 
   res.status(200).json({
     message: "Get all cars",
@@ -16,7 +15,7 @@ export const getCars =  async (req: Request, res: Response) => {
 export const getCarById = async (req: Request, res: Response) => {
   try {
     const getId: number = Number(req.params.id);
-    const car = await CarsModel.query().findById(Number(getId)).throwIfNotFound();
+    const car = await getCarId(getId);
     res.status(200).json({
       message: "Get specific car by id",
       car,
@@ -40,14 +39,14 @@ export const createCar = [mUpload.single('image'), async (req: Request, res: Res
     const {name, price, start_rent, finish_rent, availability} = req.body;
 
     // Create a new car entry with the Cloudinary image URL
-    const car = await CarsModel.query().insert({
+    const car = await postCar({
       name,
       price,
       image: result.url,
       start_rent,
       finish_rent,
       availability,
-    }).returning("*");
+    });
 
     console.log({car});
     
@@ -77,19 +76,19 @@ export const updateCar = [mUpload.single('image'), async (req: Request, res: Res
     const {name, price, start_rent, finish_rent, availability} = req.body;
     const getId: number = Number(req.params.id);
     
-    const existingCar = await CarsModel.query().findById(getId);
+    const existingCar = await getCarId(getId);
     if (!existingCar) {
       return res.status(404).json({ message: "Car not found" });
     }
 
-    const car = await CarsModel.query().findById(getId).patch({
+    const car = await putCar(getId, {
       name,
       price,
       image: result.url,
       start_rent,
       finish_rent,
       availability,
-    }).returning("*");
+    });
     
     res.status(200).json({
       message: "Update a car",
@@ -106,7 +105,7 @@ export const updateCar = [mUpload.single('image'), async (req: Request, res: Res
 export const deleteCar = async (req: Request, res: Response) => {
   try {
     const getId: number = Number(req.params.id);
-    const car = await CarsModel.query().deleteById(getId).throwIfNotFound().returning("*");
+    const car = await removeCar(getId);
     res.status(202).json({
       message: "Delete a car",
       car,
